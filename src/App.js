a726fce4c632f2e9f077a196bd4ac2abc236db98
ALEX Solutions <alexsolutions@annette-ong.local>
2019-07-29 12:39:58 +1000

    GET request

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import React, { Component } from 'react';
 import API from '../src/utils/API';
 import User from './User';
 import womanIcon from '../src/assets/images/woman-avatar.png';
+import { tsThisType } from '@babel/types';
 
 class App extends Component {
   constructor(props) {
@@ -26,10 +27,31 @@ class App extends Component {
         email={email} />;
     }
 
+  // async allows us to await certain actions inside
   async componentDidMount() {
     // Load async data.
+    let userData = await API.get('/', {
+      params: {
+        results: 1, // number of results expected
+        inc: 'name,email,picture' // details we expect
+      }
+    })
+    //parse results for easy use
+    userData = userData.data.results[0];
+
     // Update state with new data.
-    // Re-render our component.
+    const name = `${userData.name.first} ${userData.name.last}`;
+    const avatar = userData.picture.large;
+    const email = userData.email;
+
+    this.setState({
+      ...this.state, ...{
+        isLoading: false,
+        name, avatar, email
+      }
+    })
+
+    // Re-render of our component will be triggered.
   }
 }
 
